939b284c0ff48627027ae16cb5aaed151af51c12
sabbir1054 <mdsabbir1054@gmail.com>
2025-04-25 12:20:32 +0600

    team update

--- src/app/module/team/team.service.ts
+++ src/app/module/team/team.service.ts
@@ -56,8 +56,59 @@ const getAllMember = async () => {
   return result;
 };
 
+
+const updateTeam = async (
+  id: string,
+  payload:any
+) => {
+  const isPortfolioExist = await prisma.teamMembers.findUnique({
+    where: { id: id },
+  });
+  if (!isPortfolioExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'POrtfolio not found');
+  }
+
+  const deletePhoto = async (photoLink: string) => {
+    // Delete the image file from the server
+    const filePath = path.join(
+      process.cwd(),
+      'uploads',
+      path.basename(photoLink),
+    );
+    if (fs.existsSync(filePath)) {
+      try {
+        await fs.promises.unlink(filePath); // Using fs.promises.unlink for a promise-based approach
+      } catch (err) {
+        throw new ApiError(
+          httpStatus.NOT_FOUND,
+          `Failed to delete image or database record`,
+        );
+      }
+    } else {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'Image not found in the directory',
+      );
+    }
+  };
+
+  if (payload.image) {
+    if (isPortfolioExist.image) {
+      deletePhoto(isPortfolioExist.image);
+    }
+  }
+  const result = await prisma.teamMembers.update({
+    where: { id: id },
+    data: {
+      ...payload,
+    },
+  });
+  return result;
+};
+
 export const TeamServices = {
   createMember,
   deleteMember,
   getAllMember,
+  updateTeam,
 };
